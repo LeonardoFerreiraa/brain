f23ea88f7122cd381a264644a9d106befdd9854e
Leonardo Ferreira <mail@leonardoferreira.com.br>
2026-05-09 20:16:16 -0300

    feat: electron security hardening - contextIsolation, CSP, preload bridge (#19)

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,24 +1,55 @@
 import { ipcRenderer, contextBridge } from 'electron'
 
-// --------- Expose some API to the Renderer process ---------
-contextBridge.exposeInMainWorld('ipcRenderer', {
-  on(...args: Parameters<typeof ipcRenderer.on>) {
-    const [channel, listener] = args
-    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
+// --------- Expose safe typed API to the Renderer process ---------
+// Typed API bridge - no raw ipcRenderer leak
+const api = {
+  // File system
+  listDir: (path: string) => ipcRenderer.invoke('fs:list-dir', { path }),
+  readFile: (path: string) => ipcRenderer.invoke('fs:read-file', { path }),
+  writeFile: (path: string, content: string) =>
+    ipcRenderer.invoke('fs:write-file', { path, content }),
+  renameFile: (oldPath: string, newPath: string) =>
+    ipcRenderer.invoke('fs:rename-file', { oldPath, newPath }),
+  trashFile: (path: string) => ipcRenderer.invoke('shell:trash-file', { path }),
+  // Dialog
+  pickFolder: (defaultPath?: string) =>
+    ipcRenderer.invoke('dialog:pick-folder', { defaultPath }),
+  // Config
+  getConfig: () => ipcRenderer.invoke('config:get'),
+  setConfig: (config: Record<string, unknown>) =>
+    ipcRenderer.invoke('config:set', config),
+  // Watch
+  watchStart: (path: string) => ipcRenderer.invoke('fs:watch-start', { path }),
+  // External
+  openExternal: (url: string) => ipcRenderer.invoke('shell:open-external', { url }),
+  // Event listeners (main → renderer)
+  onTreeChanged: (callback: () => void) => {
+    ipcRenderer.on('fs:tree-changed', callback)
+    return () => ipcRenderer.off('fs:tree-changed', callback)
   },
-  off(...args: Parameters<typeof ipcRenderer.off>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.off(channel, ...omit)
+  onTreeEntry: (callback: (batch: unknown[]) => void) => {
+    ipcRenderer.on('fs:tree-entry', (_e, data) => callback(data))
+    return () => ipcRenderer.off('fs:tree-entry', callback)
   },
-  send(...args: Parameters<typeof ipcRenderer.send>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.send(channel, ...omit)
+  onFileChanged: (callback: (data: { path: string; mtime: number }) => void) => {
+    ipcRenderer.on('fs:file-changed', (_e, data) => callback(data))
+    return () => ipcRenderer.off('fs:file-changed', callback)
   },
-  invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.invoke(channel, ...omit)
+  onFileDeleted: (callback: (data: { path: string }) => void) => {
+    ipcRenderer.on('fs:file-deleted', (_e, data) => callback(data))
+    return () => ipcRenderer.off('fs:file-deleted', callback)
   },
+  onOpenFile: (callback: (filePath: string) => void) => {
+    ipcRenderer.on('open-file', (_e, filePath) => callback(filePath))
+    return () => ipcRenderer.off('open-file', callback)
+  },
+  onThemeChanged: (callback: (isDark: boolean) => void) => {
+    ipcRenderer.on('theme:changed', (_e, isDark) => callback(isDark))
+    return () => ipcRenderer.off('theme:changed', callback)
+  },
+}
 
-  // You can expose other APTs you need here.
-  // ...
-})
+contextBridge.exposeInMainWorld('api', api)
+
+// TypeScript type declaration for renderer
+export type Api = typeof api
